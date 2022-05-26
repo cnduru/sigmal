267f9539de7082f0d0a4d26882f5a8056a6f0c18
cnduru <cnduru111@gmail.com>
2022-05-26 22:53:54 +0200

    rewrote to use python3 and fixed file offset reading
    Signed-off-by: cnduru <cnduru111@gmail.com>

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ sigmal
 ======
 
 SigMal is a a simple system whose purpose is to detect malware based on their signatures.
+
+This is currently a work in progress
--- sigmal.py
+++ sigmal.py
@@ -1,8 +1,9 @@
-import mysql.connector    
+#import mysql.connector    
 from datetime import date, datetime, timedelta
+import sys
 
 def buildQuery(signature, name):
-    print 'building query..'
+    print('building query..')
     qstring = "INSERT INTO signatures "
     qstring += "(id, offsets, data) "
     qstring += "VALUES ("
@@ -11,21 +12,31 @@ def buildQuery(signature, name):
     qstring += "'" + signature[1] + "') "
     
     return qstring
-    
-def readFile(path):
-    return ''
-def generateOffsets():
-    return '0,1,3'
 
-def readAtOffsets(fileText):
-    return '43,13,2'
+def generateOffsets():
+    return (631,2231,5123)
+
+def readAtOffsets(path, offsets):
+    byts = []
+
+    f = open(path, 'rb')
+    content = f.read()
+
+    for offset in offsets:
+        f.seek(offset)
+        r = f.read(1)
+        byts.append(r)
+        print('read byte {} at offset {}'.format(r.hex(), offset))
+
+    f.close()
+
     
 def registerMalware(query):
     #add_employee = ("INSERT INTO signatures "
      #          "(id, offsets, data) "
       #         "VALUES ('TrojanVir', '15,26,37', '08,e8,e2')")
 
-    print 'registering malware signature in database..'
+    print('registering malware signature in database..')
     cnx = mysql.connector.connect(user='root', password='killer4',
                               host='localhost',
                               database='malware')
@@ -41,17 +52,21 @@ def registerMalware(query):
     cnx.close()
 
 def analyze(file_path):
-    print 'analyzing file at' + file_path + '..'
-    fileText = readFile(file_path)
+    print('analyzing file at {}..'.format(file_path))
     offset_list = generateOffsets()
-    offset_data = readAtOffsets(fileText)
+    offset_data = readAtOffsets(file_path, offset_list)
     signatureList = list()
     signatureList.append(offset_list)
     signatureList.append(offset_data)
-    print 'returning signatures..'
+    print('returning signatures..')
     return signatureList
     
 if __name__ == '__main__':
-    signature = analyze('path')
-    query = buildQuery(signature, 'test')
+    if len(sys.argv) != 2 :
+        print('missing filename - exiting')
+        sys.exit(-1)
+
+    file_path = sys.argv[1]
+    signature = analyze(file_path)
+    #query = buildQuery(signature, 'test')
     #registerMalware(query)
